8594024b7d00cf3a2ce22eea8042ec1644c74234
Aiden Chang <aidenchang@pcpartner.com>
2023-06-08 20:50:40 +0800

    Feat: Add LoginFragment
    Solution:
    1. As title
    
    Finder: aiden
    Author: aiden

---
+++ app/src/main/java/com/adncoding/taipeicity_parkinglot/ui/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.adncoding.taipeicity_parkinglot.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.adncoding.taipeicity_parkinglot.R
+
+class LoginFragment : Fragment() {
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    companion object {
+        fun newInstance() = LoginFragment()
+    }
+}
---
+++ app/src/main/java/com/adncoding/taipeicity_parkinglot/ui/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.adncoding.taipeicity_parkinglot.ui
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
